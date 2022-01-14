15b1c53add94a092ce51b196c72e830a4165744f
Hector Moreira <hector.moreirac@gmail.com>
2022-01-13 19:47:37 -0500

    entity Course

--- Education.Domain/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Education.Domain
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ Education.Domain/Course.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Education.Domain
+{
+    public class Course
+    {
+        [Key]
+        public Guid CourseId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Description { get; set; }
+
+        [DataType(DataType.Date)]
+        [DateInFuture]
+        public DateTime? PublicationOn { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? CreatedOn { get; set; }
+
+        public float Price { get; set; }
+    }
+}
